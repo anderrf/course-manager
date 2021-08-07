ec7cf2728f1f31b937ffd92c7a8687fc8d9f1871
anderrf <anderson_rocha_farias@outlook.com>
2021-08-07 12:54:16 -0300

    Transfer navbar into core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { CoreModule } from './core/core.module';
 import { CourseModule } from './courses/course.module';
 import { Error404Component } from './error-404/error-404.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component
   ],
   imports: [
@@ -24,7 +23,8 @@ import { HttpClientModule } from '@angular/common/http';
       }
     ]),
     HttpClientModule,
-    CourseModule
+    CourseModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
