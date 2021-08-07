6ca8bf6401efc892b5058677589af49178213bfa
anderrf <anderson_rocha_farias@outlook.com>
2021-08-07 13:07:04 -0300

    Transfer error component into core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { CoreModule } from './core/core.module';
 import { CourseModule } from './courses/course.module';
-import { Error404Component } from './error-404/error-404.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -9,22 +8,18 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
-    ]),
-    HttpClientModule,
-    CourseModule,
-    CoreModule
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
