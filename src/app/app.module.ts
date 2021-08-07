1efa6fdd4f41e23aef06ed1b1ee4951a958e11d6
anderrf <anderson_rocha_farias@outlook.com>
2021-08-07 12:22:39 -0300

    Segregation of course module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,7 @@
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
 import { Error404Component } from './error-404/error-404.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { StarComponent } from './star/star.component';
-import { CourseListComponent } from './courses/course-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
@@ -14,23 +10,12 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
@@ -38,7 +23,8 @@ import { HttpClientModule } from '@angular/common/http';
         path: '**', component: Error404Component
       }
     ]),
-    HttpClientModule
+    HttpClientModule,
+    CourseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
